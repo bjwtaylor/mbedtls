5f0aa57e601dc06510a9b090e75378edc7e9a17e
Ben Taylor <ben.taylor@linaro.org>
2026-09-14 11:52:21 +0100

    Correct error code for buffer too small
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>
    (cherry picked from commit c9de64df767cfb3b68082a9dd5ca6adf46884cb9)

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -9141,7 +9141,7 @@ int mbedtls_psa_ecjpake_read_round(
              ++step) {
             /* Length is stored at the first byte */
             if (input_offset + 1 > len) {
-                return MBEDTLS_ERR_SSL_BUFFER_TOO_SMALL;
+                return MBEDTLS_ERR_SSL_HANDSHAKE_FAILURE;
             }
             size_t length = buf[input_offset];
             input_offset += 1;
